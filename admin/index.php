75fd97a2a4e1d2dd2868cb0ad7a502d0534ea117
deyanuz <103450547+deyanuz@users.noreply.github.com>
2024-02-29 04:44:15 +0600

    admin panel design

---
+++ admin/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adminStyle.css">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <img src="logo1.png" alt="">
+                    <h2>Zuaneyd Khan</h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <i class='bx bx-x'></i>
+                </div>
+            </div>
+            <div class="sidebar">
+
+                <a href="#">
+                    <i class='bx bx-home-alt-2'></i>
+                    <h3>HOME</h3>
+                </a>
+                <a href="#" class="active">
+                    <i class='bx bx-info-circle'></i>
+                    <h3>ABOUT</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bx-book-alt'></i>
+                    <h3>EDUCATION</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bxs-widget'></i>
+                    <h3>SKILLS</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bx-laptop'></i>
+                    <h3>WORK</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bx-message-rounded'></i>
+                    <h3>MESSAGES</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bxs-contact'></i>
+                    <h3>CONTACT</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bx-cog'></i>
+                    <h3>SETTINGS</h3>
+                </a>
+                <a href="#">
+                    <i class='bx bx-log-out'></i>
+                    <h3>LOG OUT</h3>
+                </a>
+            </div>
+
+        </aside>
+    </div>
+</body>
+
+</html>
